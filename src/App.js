e17d80dfd0be77cfc585d57b235eb1ac1cbe6638
Arnaud <arnaud.lahaual@gmail.com>
2023-04-17 12:57:22 +0200

    import API et éléments

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import PokemonCollection from './components/PokemonCollection.js';
 
 function App() {
 
-  const [ pokemons, setPokemon] = useState([])
+  const [ pokemons, setPokemons] = useState([])
 
   useEffect (() => {
   const getPokemon = async () => {
     const res = await axios.get(
       "https://pokeapi.co/api/v2/pokemon?limit=20&offset=20")
-      console.log(res);
+      console.log(res.data.results)
+
+      res.data.results.forEach( async (pokemon) => {
+        const poke = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+setPokemons((poke) => [...poke, poke.data])
+      });
 }
 getPokemon()
   },[])
+
+
 
 
   return (
@@ -21,6 +29,7 @@ getPokemon()
     <header className='pokemon'>
       Pokemon
     </header>
+    <PokemonCollection />
     </div>
   );
 }
---
+++ src/components/PokemonCollection.js/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function PokemonCollection(name,url) {
+    return(
+    <div>
+        <h1>PokemonCollection</h1>
+        <p>{name}</p>
+        <p>{url}</p>
+
+
+    </div>
+    )
+}
+
+export default PokemonCollection;
