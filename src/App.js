90a6e4d0bfdc212d1fd2620b2f09962033ad2866
Arnaud <arnaud.lahaual@gmail.com>
2023-04-20 11:25:04 +0200

    création des components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import { useEffect, useState } from 'react';
 import './App.css';
-import { fetchPokemonByName, fetchPokemons } from './actions/pokemons';
-import { useDispatch } from 'react-redux';
-import PokemonCollection from './components/PokemonCollection.js';
 
 function App() {
-const dispatch=useDispatch();
-
-  useEffect(() => {
-
-    dispatch(fetchPokemonByName());
-  }, []);
 
   return (
     <div className="App">
       <h1>Pokemon</h1>
     <div className='pokemon-container'>
-      <PokemonCollection />
     </div>
     <button className='load-more'>Voir plus</button>
     </div>
